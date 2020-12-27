0729d09363fd065fdb9a8cffaf26058018c19911
Daniil <rusm9snik2013@yandex.ru>
2020-12-28 00:21:49 +0300

    part 3

--- react-to-do/src/components/AddList/index.jsx
+++ react-to-do/src/components/AddList/index.jsx
@@ -6,9 +6,22 @@ import closeSvg from '../../assets/img/close.svg';
 
 import './AddList.scss';
 
-const AddList = ({ colors }) => {
+const AddList = ({ colors, onAdd }) => {
     const [visiblePopup, setVisiblePopup] = useState(false);
     const [seletedColor, selectColor] = useState(colors[0].id);
+    const [inputValue, setInputValue] = useState('');
+
+    const addList = () => {
+        if (!inputValue) {
+            alert('Введите название списка');
+            return;
+        }
+        onAdd({
+            "id": Math.random(),
+            "name": inputValue,
+            "colorId": seletedColor
+        })
+    }
 
     return (
         <div className="add-list">
@@ -53,7 +66,14 @@ const AddList = ({ colors }) => {
                         alt="Close button"
                         className="add-list__popup-close-btn"
                     />
-                    <input className="field" type="text" placeholder="Название списка" />
+
+                    <input
+                        value={inputValue}
+                        onChange={e => setInputValue(e.target.value)}
+                        className="field"
+                        type="text"
+                        placeholder="Название списка" />
+
                     <div className="add-list__popup-colors">
                         {colors.map(color => (
                             <Badge
@@ -64,7 +84,7 @@ const AddList = ({ colors }) => {
                             />
                         ))}
                     </div>
-                    <button className="button">Добавить</button>
+                    <button onClick={addList} className="button">Добавить</button>
                 </div>
             )}
         </div>
